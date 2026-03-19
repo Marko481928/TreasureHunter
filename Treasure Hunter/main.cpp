d4a5df25b3c11378a783d3eb6924e3bf3e9bf393
marko <g00481928@atu.ie>
2026-03-19 12:57:01 +0000

    Done first task. Added 6x6 grid field with random numbers

--- Treasure Hunter/main.cpp
+++ Treasure Hunter/main.cpp
@@ -1,10 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 int main()
 {
-	printf("Hello world");
-	//add a comment
+	const int row = 6, col = 6;
+	srand((int)time(0));
+	int field[row][col];
 
-	//Just for commit
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			field[i][j] = (rand() % 255);
+			printf("%d\n", field[i][j]);
+		}
+	}
+
+
+
 	return 0;
 }
